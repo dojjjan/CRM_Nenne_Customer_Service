24ab9570bb67d9a8e33540e4bb8b3a82b4023f62
hrkonsult <hrkonsult@mskoog.se>
2020-09-25 19:09:59 +0200

    Add project files.

---
+++ CRM_Nenne_Customer_API/Models/CoreDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace CRM_Nenne_Customer_API.Models
+{
+    public partial class CoreDbContext : DbContext
+    {
+        private readonly IOptions<Settings> appSettings;
+        public CoreDbContext()
+        {
+        }
+        public CoreDbContext(IOptions<Settings> app)
+        {
+            appSettings = app;
+        }
+        public CoreDbContext(DbContextOptions<CoreDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(appSettings.Value.DbConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.CustomerAddress).IsUnicode(false);
+
+                entity.Property(e => e.CustomerContact).IsUnicode(false);
+
+                entity.Property(e => e.CustomerDescription).IsUnicode(false);
+
+                entity.Property(e => e.CustomerName).IsUnicode(false);
+
+                entity.Property(e => e.CustomerPhone).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CRM_Nenne_Customer_API/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_Nenne_Customer_API.Models
+{
+    public partial class Customer
+    {
+        public int? CustomerId { get; set; }
+        [StringLength(255)]
+        public string CustomerName { get; set; }
+        [StringLength(255)]
+        public string CustomerAddress { get; set; }
+        [StringLength(64)]
+        public string CustomerPhone { get; set; }
+        [StringLength(500)]
+        public string CustomerDescription { get; set; }
+        [StringLength(255)]
+        public string CustomerContact { get; set; }
+    }
+}
